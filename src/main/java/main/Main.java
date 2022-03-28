1e966f2b0fa854ca39508d9541fefd3aebe83fc8
ASUS <mehrandab78@gmail.com>
2022-03-28 15:28:53 +0430

    writing main for UI

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("here");
+    static Scanner input=new Scanner(System.in);
+    public static void main(String[] args)  {
+        Boolean flag=true;
+        while (flag){
+            System.out.println("1-login"+"\n"+"2-singUp"+"\n"+"3-exit");
+            Integer operator= input.nextInt();
+        }
     }
 }
