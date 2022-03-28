e4879df657d8f06ead0cb9de856f7d1799c36a33
ASUS <mehrandab78@gmail.com>
2022-03-28 14:16:03 +0430

    writing repositories

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Boolean flag=true;
+        while(flag){
+            System.out.println("1-login"+"\n"+"2-singUp"+"\n"+"3-exit");
+            flag=false;
+        }
+    }
+}
